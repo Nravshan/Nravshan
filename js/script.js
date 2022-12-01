d2a825ecfb3b36684043a26866257cf8ba675b90
Nravshan <119561541+Nravshan@users.noreply.github.com>
2022-12-01 11:40:09 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let btnMenu = document.querySelector('.burger_menu')
+let navMenu= document.querySelector('.nav')
+
+btnMenu.addEventListener('click', function name(params) {navMenu.classList.toggle('active')
+    
+})
